0d0ac3d80138d156f9fe35a1d6d7fb2153544fda
Dmitriy <dimulatoraz@gmail.com>
2023-08-05 11:12:40 +0700

    5 Redone

--- 5/5_1/server.c
+++ 5/5_1/server.c
@@ -4,59 +4,57 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int main()
-{
-	int sockfd;
-	int client, n;
-	char line[1000];
-	struct sockaddr_in servaddr, cliaddr;
-
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(51000);
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	
-	/*Create UDP socket*/
-	if((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
-	{
-		perror(NULL);
-		exit(EXIT_FAILURE);
-	}
-
-	/**/
-	if(bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
-	{
-		perror(NULL);
-		close(sockfd);
-		exit(EXIT_FAILURE);
-	}
-	
-	while(1)
-	{
-		client = sizeof(cliaddr);
-
-		if((n = recvfrom(sockfd, line, 999, 0, (struct sockaddr *) &cliaddr, client)) < 0)
-		{
-			perror(NULL);
-			close(sockfd);
-			exit(EXIT_FAILURE);
-		}
-
-		printf("%s\n", line);
-
-		if(sendto(sockfd, line, strlen(line), 0, (struct sockaddr *) &cliaddr, client) < 0)
-		{
-			perror(NULL);
-			close(sockfd);
-			exit(EXIT_FAILURE);
-
-		}
-
-		exit(EXIT_SUCCESS);
-	}
-
+int main(int argc, char **argv) {
+    if (argc != 2){
+        printf("Usage: ./server <port>\n");
+        exit(EXIT_FAILURE);
+    }
+    int sockfd; /* Дескриптор сокета */
+    int clilen, n; /* Переменные для различных длин и количества символов */
+    char line[1000]; /* Массив для принятой и отсылаемой строки */
+    struct sockaddr_in servaddr, cliaddr; /* Структуры для адресов сервера и клиента */
+    /* Заполняем структуру для адреса сервера */
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    int port = atoi(argv[1]);
+    if (port == 0) {
+        printf("Invalid port\n");
+        close(sockfd); /* По окончании работы закрываем дескриптор сокета */
+        exit(EXIT_FAILURE);
+    }
+    servaddr.sin_port = htons(port);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    /* Создаем UDP сокет */
+    if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0){
+        perror(NULL); /* Печатаем сообщение об ошибке */
+        exit(EXIT_FAILURE);
+    }
+    /* Настраиваем адрес сокета */
+    if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+        perror(NULL);
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    while(1) {
+        /* Основной цикл обслуживания*/
+        clilen = sizeof(cliaddr);
+        /* Ожидаем прихода запроса от клиента и читаем его */
+        if((n = recvfrom(sockfd, line, 999, 0, (struct sockaddr *) &cliaddr, &clilen)) < 0){
+            perror(NULL);
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+        /* Печатаем принятый текст на экране */
+        printf("%s\n", line);
+        /* Принятый текст отправляем обратно по адресу отправителя */
+        if(sendto(sockfd, line, strlen(line)+1, 0, (struct sockaddr *) &cliaddr, clilen) < 0){
+            perror(NULL);
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        } /* Уходим ожидать новую датаграмму*/
+    }
 }
